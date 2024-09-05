23d36860630d92645e21ba6313868b97cc724380
somuyakhandelwal <somuyakhandelwal@gmail.com>
2024-09-05 11:05:08 +0530

    Added this pointer usage in Vehicle and TrafficLight classes

--- src/TrafficSim.java
+++ src/TrafficSim.java
@@ -7,6 +7,7 @@ class Vehicle {
 
     // Constructor to initialize the Vehicle object
     public Vehicle(String type, int speed, String direction) {
+        // Using 'this' to refer to the current object's attributes
         this.type = type;
         this.speed = speed;
         this.direction = direction;
@@ -14,6 +15,7 @@ class Vehicle {
 
     // Member function to display the vehicle's information
     public void displayInfo() {
+        // Using 'this' to refer to the current object's methods
         System.out.println("Vehicle Type: " + this.type + ", Speed: " + this.speed + " km/h, Direction: " + this.direction);
     }
 
@@ -24,11 +26,12 @@ class Vehicle {
 
     // Getter for speed
     public int getSpeed() {
-        return speed;
+        return this.speed; // Using 'this' to access the current object's speed
     }
 
     // Setter for speed
     public void setSpeed(int speed) {
+        // Using 'this' to differentiate between parameter and attribute
         this.speed = speed;
     }
 }
@@ -41,6 +44,7 @@ class TrafficLight {
 
     // Constructor to initialize the TrafficLight object
     public TrafficLight(String color, int duration) {
+        // Using 'this' to refer to the current object's attributes
         this.color = color;
         this.duration = duration;
     }
@@ -52,22 +56,23 @@ class TrafficLight {
 
     // Member function to change the state of the traffic light
     public void changeColor(String newColor) {
-        this.color = newColor;
+        this.color = newColor; // Using 'this' to refer to the current object's color
         System.out.println("Traffic Light changed to " + this.color);
     }
 
     // Getter for color
     public String getColor() {
-        return color;
+        return this.color; // Using 'this' to return the current object's color
     }
 
     // Setter for color
     public void setColor(String color) {
+        // Using 'this' to differentiate between parameter and attribute
         this.color = color;
     }
 }
 
-// Main class to demonstrate Classes and Objects in the TrafficSim
+// Main class to demonstrate Classes, Objects, and 'this' pointer in the TrafficSim
 public class TrafficSim {
     public static void main(String[] args) {
         // Creating an object of the Vehicle class
@@ -75,7 +80,7 @@ public class TrafficSim {
         car.displayInfo(); // Display vehicle info
         car.move();        // Simulate vehicle movement
         
-        // Adjusting the speed of the car
+        // Adjusting the speed of the car using 'this' pointer inside the setSpeed method
         car.setSpeed(80);
         System.out.println("Updated speed of the car: " + car.getSpeed() + " km/h");
 
@@ -83,7 +88,7 @@ public class TrafficSim {
         TrafficLight light = new TrafficLight("Red", 30);
         light.displayState(); // Display traffic light state
 
-        // Changing the traffic light's color
+        // Changing the traffic light's color using 'this' pointer inside the changeColor method
         light.changeColor("Green");
         light.displayState(); // Display updated state
     }
